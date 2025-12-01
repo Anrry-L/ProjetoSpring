cefdb7149b670868f11a5e4640f6647703797049
anrry <anrrylauan3@gmail.com>
2025-12-01 14:43:48 -0300

    Lombok implementado, criado getters and setters e construtores

--- src/main/java/dev/Obi/ProjetoSpring/Missoes/MissoesModel.java
+++ src/main/java/dev/Obi/ProjetoSpring/Missoes/MissoesModel.java
@@ -2,10 +2,16 @@ package dev.Obi.ProjetoSpring.Missoes;
 
 import dev.Obi.ProjetoSpring.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 @Table(name = "tb_missoes")
 public class MissoesModel {
 
